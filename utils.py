ab0de1b21533ecb39657b0cdce3e8d4f8e42c83f
ericagreen <chenjuanhello@hotmail.com>
2020-10-22 12:14:09 -0400

    add all

--- utils.py
+++ utils.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-
 import csv
 import emoji
 import pandas as pd
@@ -13,15 +10,25 @@ import tarfile
 import wget
 
 from typing import NamedTuple, Tuple, Dict
+from pandas import DataFrame
+from functools import partial
+
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 DataMeta = NamedTuple('DataMeta', [
     ('name', str), ('url', str)])
+
 
 _GLOVE = DataMeta(
     name='glove',
     url='https://www.dropbox.com/s/g5pkso42wq2ipti/glove.tar.gz?dl=1'
 )
 
+_EMOJI_MODEL = DataMeta(
+    name='emoji_model_best',
+    url='https://www.dropbox.com/s/93frkdrhs33ihfz/emoji_model_best.tar.gz?dl=1'
+) 
 
 def _download_data_if_needed(data_meta: DataMeta) -> str:
     """
@@ -45,6 +52,23 @@ def _download_data_if_needed(data_meta: DataMeta) -> str:
         print('data files are now available at %s' % path)
     return path
 
+
+def download_best_emoji_model() -> Tuple[str, str]:
+    """
+    Download pretrained emoji model, skip if already downloaded,
+    return the path to the network json and weights
+    """
+    path = _download_data_if_needed(_EMOJI_MODEL)
+    return os.path.join(path, 'network.json'), os.path.join(path, 'weights.h5')
+
+
+def _get_train_test_df(data_meta: DataMeta) -> Tuple[DataFrame, DataFrame]:
+    path = _download_data_if_needed(data_meta)
+    train, test = tuple(
+        pd.read_csv(os.path.join(path, file))
+        for file in ['train.csv', 'test.csv'])
+    print('{} loaded successfully.'.format(data_meta.name))
+    return train, test
 
 def load_glove_vecs() -> Tuple[Dict[str, int], Dict[str, np.array]]:
     """
@@ -72,30 +96,6 @@ def load_glove_vecs() -> Tuple[Dict[str, int], Dict[str, np.array]]:
             i = i + 1
     print('glove loaded successfully.')
     return word_to_index, word_to_vec
-
-
-def load_moon():
-    '''Load moon dataset from sklearn'''
-    X, Y = datasets.make_moons(n_samples=1000, noise=0.1, random_state=1234)
-    Y = Y.reshape(Y.shape[0], 1)
-    return train_test_split(X, Y, test_size=0.2, random_state=1234)
-
-
-def plot_decision_boundary(model, X, y):
-    xmin, xmax = X[:, 0].min() - 1, X[:, 0].max() + 1
-    ymin, ymax = X[:, 1].min() - 1, X[:, 1].max() + 1
-
-    h = 0.01
-    xx, yy = np.meshgrid(
-        np.arange(xmin, xmax, h),
-        np.arange(ymin, ymax, h)
-    )
-
-    logits = model.predict(np.c_[xx.ravel(), yy.ravel()])
-    Z = np.where(logits >= 0.5, 1, 0).reshape(xx.shape)
-
-    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral, alpha=0.8)
-    plt.scatter(X[:, 0], X[:, 1], c=y.T[0], cmap=plt.cm.Spectral)
 
 
 def load_emoji():
@@ -146,6 +146,32 @@ def label_to_emoji(label):
     return emoji.emojize(EMOJI_DICT[str(label)], use_aliases=True)
 
 
+def sentences_to_indices(X, word_to_index, max_len):
+    """
+    Return a array of indices of a given sentence.
+    The sentence will be trimed/padded to max_len
+
+    Args:
+        X (np.ndarray): Input array of sentences, the shape is (m,)  where m is the number of sentences, each sentence is a str. 
+        Example X: array(['Sentence 1', 'Setence 2'])
+        word_to_index (dict[str->int]): map from a word to its index in vocabulary
+
+    Return:
+        indices (np.ndarray): the shape is (m, max_len) where m is the number of sentences
+    """
+    m = X.shape[0]
+    X_indices = np.zeros((m, max_len))
+    for i in range(m):
+        sentence_words = X[i].lower().split()
+        j = 0
+        for w in sentence_words:
+            X_indices[i, j] = word_to_index[w]
+            j = j + 1
+    return X_indices
+
+
+
+
 def sentence_to_avg(sentence, word_to_vec_map):
     import numpy as np
 
@@ -160,3 +186,29 @@ def sentence_to_avg(sentence, word_to_vec_map):
     avg = avg / cnt
 
     return avg
+
+
+
+def load_moon():
+    '''Load moon dataset from sklearn'''
+    X, Y = datasets.make_moons(n_samples=1000, noise=0.1, random_state=1234)
+    Y = Y.reshape(Y.shape[0], 1)
+    return train_test_split(X, Y, test_size=0.2, random_state=1234)
+
+
+def plot_decision_boundary(model, X, y):
+    xmin, xmax = X[:, 0].min() - 1, X[:, 0].max() + 1
+    ymin, ymax = X[:, 1].min() - 1, X[:, 1].max() + 1
+
+    h = 0.01
+    xx, yy = np.meshgrid(
+        np.arange(xmin, xmax, h),
+        np.arange(ymin, ymax, h)
+    )
+
+    logits = model.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = np.where(logits >= 0.5, 1, 0).reshape(xx.shape)
+
+    plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral, alpha=0.8)
+    plt.scatter(X[:, 0], X[:, 1], c=y.T[0], cmap=plt.cm.Spectral)
+
